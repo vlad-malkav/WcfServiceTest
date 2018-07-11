c1ff2a0f774d46732b4d8d416e08cff5ac303971
vlad-malkav <allanlerouge@gmx.fr>
2018-07-11 21:30:47 +0200

    Add project files.

---
+++ WcfServiceTest/BddAccessTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace WcfServiceTest
+{
+    public static class BddAccessTest
+    {
+        public static int SQLAvecRetourInt(string commande)
+        {
+            string connectionString =
+                @"Server=mysql-allanlerouge.alwaysdata.net;Port=3306;Database=allanlerouge_test;Uid=154607_test;Password=commelesang;CHARSET=utf8;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader reader;
+
+            string StringID = "";
+            int IntegerID;
+            try
+            {
+                connection.Open();
+                command.CommandText = commande;
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    StringID = reader[0].ToString();
+                }
+
+                Int32.TryParse(StringID, out IntegerID);
+                if (IntegerID != -1)
+                {
+                    connection.Close();
+                    return IntegerID;
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return -1;
+        }
+
+        public static string SQLAvecRetourString(string commande)
+        {
+            string connectionString =
+                @"Server=mysql-allanlerouge.alwaysdata.net;Port=3306;Database=allanlerouge_test;Uid=154607_test;Password=commelesang;CHARSET=utf8;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader reader;
+
+            string result = "fail";
+            try
+            {
+                connection.Open();
+                command.CommandText = commande;
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    result = reader[0].ToString();
+                }
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return result;
+        }
+
+        public static void SQLSansRetour(string commande)
+        {
+            string connectionString =
+                @"Server=mysql-allanlerouge.alwaysdata.net;Port=3306;Database=allanlerouge_test;Uid=154607_test;Password=commelesang;CHARSET=utf8;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
+            try
+            {
+                connection.Open();
+                command.CommandText = commande;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+            connection.Close();
+        }
+
+        public static string TestSimple(int id)
+        {
+            SQLSansRetour("INSERT INTO table_test (id,data1,data2,data3) VALUES (" + id + ",'data1_" + id + "','data2_" + id + "','data3_" + id + "')");
+            int ID = SQLAvecRetourInt("SELECT id FROM table_test WHERE data1='data1_" + id + "'");
+
+            return ID.ToString();
+        }
+
+        public static string TestComplet(int id, string data1, string data2, string data3)
+        {
+            SQLSansRetour("INSERT INTO table_test (id,data1,data2,data3) VALUES (" + id + ", '" + data1 + "', '" + data2 + "', '" + data3 + "')");
+            return SQLAvecRetourString("SELECT data1 FROM table_test WHERE id=" + id);
+        }
+    }
+}
---
+++ WcfServiceTest/WebServiceTest.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WcfServiceTest
+{
+    /// <summary>
+    /// Summary description for WebServiceTest
+    /// </summary>
+    [WebService(Namespace = "http://amococadiz.somee.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceTest : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string toto)
+        {
+            return "Hello " + toto + "'s World";
+        }
+
+        [WebMethod]
+        public string Test(int id)
+        {
+            return BddAccessTest.TestSimple(id);
+        }
+
+        [WebMethod]
+        public string TestComplet(int id, string data1, string data2, string data3)
+        {
+            return BddAccessTest.TestComplet(id, data1, data2, data3);
+        }
+    }
+}
